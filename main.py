6b3778dea3b39e46a3c098c795088192c70aa998
Aldon Smith <smithaldon@gmail.com>
2022-06-06 21:00:42 -0400

    updates made to abstract view from business logic.

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
 import sys
-from PySide6.QtWidgets import *
-from PySide6.QtCore import *
-from PySide6.Qt import *
-from PySide6.QtGui import *
-
-class MainWindow(QMainWindow):
-    """
-    Class definition for the main window
-    @function: __init__(self) --> window initialization
-    @function: 
-    """
-    def __init__(self):
-        super().__init__()
-
-        # Window Properties
-        self.setWindowTitle("Todo App | PySide6")
-        self.setMinimumSize(QSize(800, 600))
-
-        l = QLabel("Main Window")
-        l.setMargin(10)
-        self.setCentralWidget(l)
-        self.show()
+from PySide6.QtWidgets import QApplication
+from PySide6.QtQuick import QQuickView
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = MainWindow()
-    app.exec()
+    app = QApplication()
+    view = QQuickView()
+    view.setSource("view.qml")
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+    view.show()
+    sys.exit(app.exec())
---
+++ view.qml
@@ -0,0 +1,13 @@
+import QtQuick
+
+Rectangle {
+    id: main
+    width: 800
+    height: 600
+    color: "green"
+
+    Text {
+        text: "To-Do App Main Page"
+        anchors.centerIn: main
+    }
+}
